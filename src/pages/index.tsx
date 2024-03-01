648f726e32795b4a557eaaaeb32ec4c003e4021f
rania <raniamn03@gmail.com>
2024-03-01 18:29:21 +0700

    tugas hari-10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-import { Layout } from "@/layout";
 import { useEffect } from "react";
+import Image from 'next/image';
+import dynamic from "next/dynamic";
+
+const LayoutComponent = dynamic(
+  () => import('@/layout').then(mod => mod.Layout)
+);
 
 export default function Home() {
   useEffect(() => {
@@ -9,14 +14,27 @@ export default function Home() {
       .catch((err) => console.log("err => ", err));
   }, [])
   return (
-    <Layout metaTitle="Home" metaDescription="Welcome to Raniaarn's App">
-      <div className="flex flex-col">
-        <div className="flex-grow">
-          <div className="px-2 py-8 text-xl font-bold">
-            Welcome to Raniaarn's App!
+    <>
+      <LayoutComponent metaTitle="Home" metaDescription="Welcome to Raniaarn's App">
+        <div className="flex flex-col">
+          <div className="flex-grow">
+            <div className="px-2 py-8 text-xl font-bold">
+              Welcome to Raniaarn's App!
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  )
+        <div className="flex justify-center">
+          <div className="w-screen h-screen p-4 mb-16">
+            <Image
+              src="/landing.png"
+              layout="responsive"
+              width={1440}
+              height={700}
+              objectFit="contain"
+              alt="raniaarn img" />
+          </div>
+        </div>
+      </LayoutComponent>
+    </>
+  );
 }
